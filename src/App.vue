5cfdf35f3c5aed95c3075580ed3ce9998b23c071
羁绊 <347300979@qq.com>
2020-06-24 22:10:22 +0800

    composition Api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
+<!--
+ * @Author: RONGWEI PENG
+ * @Date: 2020-06-24 21:45:56
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2020-06-24 22:10:17
+ * @FilePath: /vite-demo-project/src/App.vue
+-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>
+    <h1 v-show="y > 360">{{ count }}* 2 = {{ double }}</h1>
+    <p>{{ x }}:{{ y }}</p>
+    <button @click="add">add</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import { ref, watchEffect, onMounted, onUnmounted, computed } from 'vue';
+// composition Api
+function useMouse() {
+  let x = ref(0),
+    y = ref(0);
+  function update(e) {
+    x.value = e.pageX;
+    y.value = e.pageY;
   }
+
+  onMounted(() => {
+    window.addEventListener('mousemove', update);
+  });
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', update);
+  });
+
+  return { x, y };
 }
+export default {
+  setup() {
+    let count = ref(0);
+    let add = () => {
+      count.value++;
+    };
+    let double = computed(() => count.value * 2);
+    let { x, y } = useMouse();
+    watchEffect(() => {
+      console.log('count.value++', count.value);
+    });
+    return { count, add, double, x, y };
+  },
+};
 </script>
+
+<style scoped></style>
